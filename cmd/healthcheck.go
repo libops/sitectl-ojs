libops/sitectl-omeka-classic

Remove baked-in homebrew dependencies

cmd/healthcheck.go
@@ -1,0 +1,30 @@
+package cmd
+
+import (
+	"github.com/libops/sitectl/pkg/config"
+	"github.com/libops/sitectl/pkg/healthcheck"
+	"github.com/libops/sitectl/pkg/plugin"
+	sitevalidate "github.com/libops/sitectl/pkg/validate"
+	"github.com/spf13/cobra"
+)
+
+type omekaClassicHealthcheckRunner struct{}
+
+func (omekaClassicHealthcheckRunner) BindFlags(cmd *cobra.Command) {}
+
+func (omekaClassicHealthcheckRunner) Run(cmd *cobra.Command, ctx *config.Context) ([]sitevalidate.Result, error) {
+	results := []sitevalidate.Result{
+		healthcheck.CheckHTTP(cmd.Context(), "http:omeka-classic", healthcheck.PublicURLFromEnv(ctx, "http", "localhost")),
+	}
+
+	checker, err := healthcheck.NewDockerChecker(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = checker.Close() }()
+
+	results = append(results, checker.CheckMariaDB(cmd.Context(), "mariadb"))
+	return results, nil
+}
+
+var _ plugin.HealthcheckRunner = omekaClassicHealthcheckRunner{}

cmd/root.go
@@ -7,7 +7,6 @@ const (
 	createBranch = "main"
 	pluginName   = "omeka-classic"
 	defaultPath  = "./omeka-classic"
-	displayName  = "Omeka Classic"
 )
 
 func createDefinition() plugin.CreateSpec {
@@ -34,11 +33,11 @@ func RegisterCommands(s *plugin.SDK) {
 		RequiredServices: []string{"omeka-classic"},
 		Reason:           "omeka-classic service",
 	})
-	s.RegisterStandardComposeTemplate(createDefinition(), plugin.StandardComposeTemplateOptions{
+	s.RegisterComposeTemplateCreateRunner(createDefinition(), plugin.ComposeTemplateCreateOptions{
 		DefaultPath:   defaultPath,
 		DefaultPlugin: pluginName,
 		ReadyMessage:  "Omeka Classic is ready for use through sitectl.",
-		DisplayName:   displayName,
 	})
+	s.RegisterHealthcheckRunner(omekaClassicHealthcheckRunner{})
 	registerOmekaClassicCommands(s)
 }

libops/sitectl-ojs

cmd/healthcheck.go
@@ -1,0 +1,30 @@
+package cmd
+
+import (
+	"github.com/libops/sitectl/pkg/config"
+	"github.com/libops/sitectl/pkg/healthcheck"
+	"github.com/libops/sitectl/pkg/plugin"
+	sitevalidate "github.com/libops/sitectl/pkg/validate"
+	"github.com/spf13/cobra"
+)
+
+type ojsHealthcheckRunner struct{}
+
+func (ojsHealthcheckRunner) BindFlags(cmd *cobra.Command) {}
+
+func (ojsHealthcheckRunner) Run(cmd *cobra.Command, ctx *config.Context) ([]sitevalidate.Result, error) {
+	results := []sitevalidate.Result{
+		healthcheck.CheckHTTP(cmd.Context(), "http:ojs", healthcheck.PublicURLFromEnv(ctx, "http", "localhost")),
+	}
+
+	checker, err := healthcheck.NewDockerChecker(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = checker.Close() }()
+
+	results = append(results, checker.CheckMariaDB(cmd.Context(), "mariadb"))
+	return results, nil
+}
+
+var _ plugin.HealthcheckRunner = ojsHealthcheckRunner{}

cmd/root.go
@@ -7,7 +7,6 @@ const (
 	createBranch = "main"
 	pluginName   = "ojs"
 	defaultPath  = "./ojs"
-	displayName  = "OJS"
 )
 
 func createDefinition() plugin.CreateSpec {
@@ -34,11 +33,11 @@ func RegisterCommands(s *plugin.SDK) {
 		RequiredServices: []string{"ojs"},
 		Reason:           "ojs service",
 	})
-	s.RegisterStandardComposeTemplate(createDefinition(), plugin.StandardComposeTemplateOptions{
+	s.RegisterComposeTemplateCreateRunner(createDefinition(), plugin.ComposeTemplateCreateOptions{
 		DefaultPath:   defaultPath,
 		DefaultPlugin: pluginName,
 		ReadyMessage:  "OJS is ready for use through sitectl.",
-		DisplayName:   displayName,
 	})
+	s.RegisterHealthcheckRunner(ojsHealthcheckRunner{})
 	registerOJSCommands(s)
 }