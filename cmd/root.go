28e1b2356202bcc0547f11c8adf21e2bb0ceee76
H.E. Pennypacker <115990865+pennypacker-he@users.noreply.github.com>
2026-07-04 01:52:03 +0000

    Create secrets directory before compose init
    Ensure lifecycle commands create ./secrets before running the init service so local Docker does not need to create translated bind-mount paths.
    
    More Info
    - Add the preflight mkdir to create and rollout lifecycle commands.

--- cmd/root.go
+++ cmd/root.go
@@ -35,6 +35,7 @@ func createDefinition() plugin.CreateSpec {
 			{Service: "ojs", Image: "libops/ojs:nginx-1.30.3-php84", BuildPolicy: plugin.BuildPolicyIfNotPresent},
 		},
 		DockerComposeInit: []string{
+			"mkdir -p ./secrets",
 			"docker compose run --rm init",
 		},
 		InitArtifacts: []plugin.InitArtifact{
@@ -58,6 +59,7 @@ func createDefinition() plugin.CreateSpec {
 		DockerComposeRollout: []string{
 			"docker compose pull --ignore-buildable --quiet || docker compose pull --ignore-buildable || true",
 			"docker compose build --pull",
+			"mkdir -p ./secrets",
 			"docker compose run --rm init",
 			"docker compose up --remove-orphans --wait --pull missing --quiet-pull -d",
 			"docker compose exec -T ojs php tools/upgrade.php upgrade || echo \"OJS database upgrade skipped or failed\"",
